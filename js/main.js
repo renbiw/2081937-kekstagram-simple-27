9f97c85c8412dbdea62b9c42134ca09310b27c6d
lena <renbiwlen@gmail.com>
2022-09-28 22:16:47 +0300

    renamed const

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function getRandomInRange(min, max) {
   const isValid = min < 0 || max < 0;
-  const revertMax = min;
-  const revertMin = max;
+  const reverseMax = min;
+  const reverseMin = max;
   if (isValid) {
     throw new RangeError('Число должно быть из диапазона неотрицательных чисел');
   }
   if (min > max) {
-    return Math.floor(Math.random() * (revertMax - revertMin + 1)) + revertMin;
+    return Math.floor(Math.random() * (reverseMax - reverseMin + 1)) + reverseMin;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
