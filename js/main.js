c6f772c8458c3dd35f98def45ef65fdfe822d713
lena <renbiwlen@gmail.com>
2022-09-28 22:15:05 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-// основа функции getRandomInRange(min, max) из источников https://myrusakov.ru/js-random-numbers.html
-
 function getRandomInRange(min, max) {
-  let isValid = (min < 0) || (max < 0);
+  const isValid = min < 0 || max < 0;
+  const revertMax = min;
+  const revertMin = max;
   if (isValid) {
     throw new RangeError('Число должно быть из диапазона неотрицательных чисел');
   }
   if (min > max) {
-    let a = min;
-    let b = max;
-    return Math.floor(Math.random() * (a - b + 1)) + b;
+    return Math.floor(Math.random() * (revertMax - revertMin + 1)) + revertMin;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function compareLength (a, len) {
-  return len >= a.length
+function compareLength (line, len) {
+  return len >= line.length
 }
 
 getRandomInRange(1, 12);
